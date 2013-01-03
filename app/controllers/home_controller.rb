f72283242775ee6532dfd1a4cd4c66d366c5a03c
FallenSquirrel <spl.hunter@gmx.net>
2013-01-03 14:29:36 +0100

    visual changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ class HomeController < ApplicationController
   def new
   end
   def index
+    if current_user
+      init_displayed_user(current_user.id)
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -62,4 +62,28 @@ module ApplicationHelper
       # end
     end
   end
+  
+  def major_block(title, date=nil, &block)
+    content_tag(:div, :class => "major_block_wrapper") do
+      concat major_block_headline(title, date)
+      concat(content_tag(:div, :class => "major_block_text") do
+        concat capture(&block)
+      end)
+    end
+  end
+  
+  def major_block_headline(title, date=nil)
+    content_tag(:div, :class => "major_block_headline") do
+      concat(content_tag(:span, :class => "major_block_title") do
+        concat title
+      end)
+      if date then
+        concat(content_tag(:span, :class => "major_block_date") do
+          concat date
+        end)
+      end
+      concat clear_float
+    end
+  end
+  
 end
